812a0562afa56ea1a7caa1a71d7a38bb2aaf6855
Jinyixuan239 <1596021045@qq.com>
2020-04-28 23:15:41 +0800

    '我的作业代码'

--- lib/student.dart
+++ lib/student.dart
@@ -1,3 +1,19 @@
-int calculate() {
-  return 6 * 7;
+class Student
+{
+  var firstName,lastName;
+  var subjectScores={'subject1':80,'subject2':87,'subject3':82};
+  Student(this.firstName,this.lastName);
+
+  String studentFullName()
+  {
+    return firstName+' '+lastName;
+  }
+  
+  double averageScore()
+  {
+    var sum=0;
+    for(var i=0;i<subjectScores.keys.toList().length;i++)
+    sum+=subjectScores.values.toList()[i];
+    return sum/subjectScores.length;
+  }
 }
