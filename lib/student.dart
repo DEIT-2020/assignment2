4385add3de370455cb697fceaed368d61475c704
ZHANGYUEYING713 <2426682361@qq.com>
2020-05-04 00:02:59 +0800

    我的作业代码

--- lib/student.dart
+++ lib/student.dart
@@ -1,3 +1,23 @@
-int calculate() {
-  return 6 * 7;
+class Student
+{
+  String firstName;
+  String lastName;
+  Student(this.firstName,this.lastName);
+  String studentFullName()
+  {
+    return '$firstName $lastName';
+  }
+
+  var subjectScores={'subject1':80,'subject2':87,'subject3':82};
+
+  double averageScore()
+  {
+    int sum=0;
+    int i;
+    for(i=0;i<subjectScores.length;i++)
+    {
+    sum+=subjectScores.values.toList()[i];
+    }
+    return  sum/i;
+  }
 }
