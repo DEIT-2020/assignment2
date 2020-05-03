a249a1ac176002b883ee1c8a57b82bf830c46df9
LeiChaoJing <lily_20110901@126.com>
2020-05-03 22:59:37 +0800

    我的作业代码

--- lib/student.dart
+++ lib/student.dart
@@ -1,3 +1,11 @@
-int calculate() {
-  return 6 * 7;
+class Student{
+  var firstName, lastName;
+  var subjectScores={'subject1':80,'subject2':87,'subject3':82};
+  Student(this.firstName,this.lastName);
+  double averageScore(){
+    return ((subjectScores['subject1']+subjectScores['subject2']+subjectScores['subject3'])/3);
+  }
+  String studentFullName(){
+  return firstName+' '+ lastName;
+  } 
 }
