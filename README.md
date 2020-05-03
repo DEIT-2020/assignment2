4e4ceca74fa9c0174d814b984007e2bc2a5174a9
shaoxingren <18019164521@163.com>
2020-05-03 22:00:09 +0800

    my_homework

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # assignment2
 dart基本知识作业 2 
+
---
+++ lib/student.dart
@@ -0,0 +1,14 @@
+class Student{
+var firstName,lastName;
+var subjectScores={'subject1':80,'subject2':87,'subject3':82};
+Student(this.firstName,this.lastName);
+double averageScore(){
+return(subjectScores['subject1']+subjectScores['subject2']+subjectScores['subject3'])/3;
+
+}
+
+String studentFullName(){
+return '$firstName $lastName';
+}
+}
+
