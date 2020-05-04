f80d528e30a0246494aa1353e25c9cee18a6d9a0
gengjing-b <gengjing0513@163.com>
2020-05-04 19:00:51 +0800

    我的作业代码

--- bin/student.dart
+++ bin/student.dart
@@ -1,14 +1,14 @@
 import 'package:assignment2/assignment2.dart' as assignment2;
 
-  int Student(String firstName,String lastName){
+  void student(List<String> arguments){
     String firstName='zhang';
     String lastName='san';
-    String studentFullName=firstName+lastName;
+    String wholeName=firstName+lastName;
     Map<String,dynamic> subjectScores={"subject1":80,"subject2":87,"subject3":82};
     var s1=subjectScores['subject1'];
     var s2=subjectScores['subject2'];
     var s3=subjectScores['subject3'];
-    var averageScores=averageScore(s1,s2,s3);
+    var averagescore=averageScore(s1,s2,s3);
   }
   int averageScore(s1,s2,s3){
     return (s1+s2+s3)/3;
--- test/main_test.dart
+++ test/main_test.dart
@@ -1,5 +1,51 @@
-import 'package:assignment2/assignment2.dart' as assignment2;
+import 'package:assignment2/student.dart';
+import 'dart:mirrors';
+import 'package:test/test.dart';
 
-main(List<String> arguments) {
-  print('Hello world: ${assignment2.calculate()}!');
-}
+
+void main(){
+  InstanceMirror studentInstancemirror=
+      reflect(Student('zhang','san'));
+  var cm=reflectClass(Student);
+  var instance =reflect(Student);
+  var type=studentInstancemirror.type;
+
+  test('has LastName field', (){
+    var lastNameMember =type.instanceMembers[#lastName];
+    expect(lastNameMember !=null, true);
+  });
+  test('has firstName field',(){
+    var firstNameMember=type.instanceMembers[#firstname];
+    expect(firstNameMember !=null, true);
+  });
+  test('has a function named studentFullName', (){
+    var studentFullName=type.instanceMembers[#studentFullName];
+    expect(studentFullName !=null, true);
+  });
+  test('has a function named averageScore',(){
+    var averageScore=type.instanceMembers[#averageScore];
+    expect(averageScore !=null, true);
+  });
+  test('has a map named sujectScores', (){
+    var subjectScores=type.instanceMembers[#subjectScores];
+    expect(subjectScores !=null, true);
+  });
+  test('average score is right',(){
+    var subjectScores =type.instanceMembers[#subjectScores];
+    var averageScores;
+    if (subjectScores!=null){
+       var resultMirror =studentInstancemirror.invoke(#averageScore,[]);
+       averageScores =resultMirror.reflectee;
+    }
+    expect(averageScores ==83.0, true);
+  });
+  test('student full name is right', (){
+    var studentFullName =type.instanceMembers[#studentFullName];
+    var fullName;
+    if (studentFullName !=null){
+       var resultMirror =studentInstancemirror.invoke(#studentFullName, []);
+       fullName=resultMirror.reflectee;
+    }
+    expect(fullName=='zhang san', true);
+  });
+  }
