887b4587893278b54a100b61967637d97714debd
chenyanjun904553829 <904553829@qq.com>
2020-05-04 17:26:50 +0800

    我的作业代码

--- lib/student.dart
+++ lib/student.dart
@@ -1,3 +1,14 @@
-int calculate() {
-  return 6 * 7;
+class Student{
+String firstName;
+String lastName;
+var subjectScores={'subject1':80,'subject2':87,'subject3':82};
+double averageScore(){
+
+  return (subjectScores['subject1']+subjectScores['subject2']+subjectScores['subject3'])/3;
+  }
+  
+   String studentFullName(){
+      return firstName+' '+lastName;
+   }
+    Student(this.firstName,this.lastName);
 }
