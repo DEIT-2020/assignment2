52b6a58e389ab94ab60b96d03c1924631e154a63
ECNUyuminqi <ymq-2000@163.com>
2020-05-04 16:16:28 +0800

    我的作业代码

---
+++ lib/student.dart
@@ -0,0 +1,13 @@
+class Student{
+  String firstName;
+  String lastName;
+  Student(this.firstName,this.lastName);
+  String studentFullName(){
+    return '$firstName'+' '+'$lastName';
+  }
+  Map<String,int> subjectScores = {'subject1':80,'subject2':87,'subject3':82};
+  double averageScore(){
+    int average = subjectScores['subject1']+subjectScores['subject2']+subjectScores['subject3'];
+    return average/3;
+  }
+}
