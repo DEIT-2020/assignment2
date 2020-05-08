701e1b289612433bec6ef5b95423871080f341b8
liujunhong-ecnu <63724126+liujunhong-ecnu@users.noreply.github.com>
2020-05-08 15:33:22 +0800

    Update student.dart

--- lib/student.dart
+++ lib/student.dart
@@ -1,3 +1,9 @@
-int calculate() {
-  return 6 * 7;
+class Student {
+  var firstName,lastName;
+  var subjectScores={'subject1':80,'subject2':87,'subject3':82};
+  Student(this.firstName,this.lastName);
+  double averageScore(){
+    var totalScores=subjectScores['subject1']+subjectScores['subject2']+subjectScores['subject3'];
+    return totalScores/3;
+  }
 }
