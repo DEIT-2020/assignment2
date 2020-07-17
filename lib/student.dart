fdd24ec014da00e21a9969f9acf4f44659f9f241
yujinyin <yujinyin0219@126.com>
2020-07-17 20:45:45 +0800

    作业

---
+++ lib/student.dart
@@ -0,0 +1,12 @@
+class Student {
+  String firstName;
+  String lastName;
+  Student(this.firstName,this.lastName);
+  var subjectScores={'subject1':80,'subject2':87,'subject3':82};
+  double averageScore(){
+    return (subjectScores['subject1']+subjectScores['subject2']+subjectScores['subject3'])/3;
+  }
+  String studentFullName(){
+    return '$firstName $lastName';
+  }
+}
